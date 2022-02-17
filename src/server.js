80ee3c141390fc544003e5c1f536a2b6a570b152
Benja272 <bm0130647@gmail.com>
2022-02-17 16:13:09 -0300

    commit ultimo

--- src/server.js
+++ src/server.js
@@ -3,9 +3,13 @@ var morgan =  require('morgan');
 const path = require('path');
 const handlebars = require('express-handlebars');
 const methodOverride = require('method-override');
+const flash = require("connect-flash")
+const sesion  = require("express-session")
+const passport = require("passport")
 
-
+//initializations
 var app = express();
+require("./config/passport")  
 
 //settings
 app.set('port', process.env.PORT || 4000);
@@ -22,10 +26,24 @@ app.set('view engine', '.hbs');
 //middlewares
 app.use(express.json()); //usar middleware para que express entienda formato json
 app.use(express.urlencoded({extended: false}))
+app.use(sesion({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash()); 
 // parse application/x-www-form-urlencoded, basically can only parse incoming Request Object if strings or arrays
 //sirve para ejecutar cualquier codigo antes de que las peticiones lleguen a su manejador
 app.use(morgan('dev'));
 app.use(methodOverride('_method'));
+
+//global variables
+app.use((req,res,next) => {
+    res.locals.success_msg = req.flash("success_msg") //devuelve el valor actual de succes_msg
+    next();
+}); 
 
 //rutes
 app.get('/', (req,res) => {
@@ -34,6 +52,7 @@ app.get('/', (req,res) => {
 
 app.use(require('./routes/indexRoutes'));
 app.use(require('./routes/Person'));
+app.use(require('./routes/users'));
 //static files 
 
 app.use(express.static(path.join(__dirname,'public')));     
